31c56b3412d7544834fc25d178bc8860442fe78e
dennisfofie <dennisbeckham000@gmail.com>
2022-06-30 12:26:36 -0700

    final commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-
-/*
-main entry
-return 0 to the std output */
-
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: always 0
+ *
+ *     */
 int main(void)
 {
-	printf ("sizeof of char: %d byte(s)\n", sizeof(char));
-	printf ("sizeof of an int: %d byte(s)\n", sizeof(int));
-	printf ("sizeof of long int: %d byte(s)\n", sizeof(long int));
-	printf ("sizeof of long long int: %d byte(s)\n", sizeof(long long int));
-	printf ("sizeof of float: %d byte(s)\n", sizeof(float));
+	printf("sizeof of a char: %d byte(s)\n", sizeof(char));
+	printf("sizeof of an int: %d byte(s)\n", sizeof(int));
+	printf("sizeof of a long int: %d byte(s)\n", sizeof(long int));
+	printf("sizeof of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("sizeof of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 
 
