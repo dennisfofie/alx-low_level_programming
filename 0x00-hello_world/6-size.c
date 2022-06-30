07d77cea3466fd737d12952bc838326e2214f302
dennisfofie <dennisbeckham000@gmail.com>
2022-06-30 12:07:43 -0700

    finally than with the fixing of any non working codes.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+main entry
+return 0 to the std output */
+
+int main(void)
+{
+	printf ("sizeof of char: %d byte(s)\n", sizeof(char));
+	printf ("sizeof of an int: %d byte(s)\n", sizeof(int));
+	printf ("sizeof of long int: %d byte(s)\n", sizeof(long int));
+	printf ("sizeof of long long int: %d byte(s)\n", sizeof(long long int));
+	printf ("sizeof of float: %d byte(s)\n", sizeof(float));
+	return (0);
+
+
+}
