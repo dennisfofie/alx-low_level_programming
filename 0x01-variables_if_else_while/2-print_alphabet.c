bbb5a9ed1c1c53f517abaeff2d85b1f5f52b7c20
dennisfofie <dennisbeckham000@gmail.com>
2022-07-01 09:09:34 -0700

    printing the lower cases of the alphabet to the screen

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/* adding more headers */
+
+/* defining a function main */
+/* entry point = main */
+	int main(void)
+{
+/* initializing a variable a */
+		char a;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+/* return : values 0 */
+		return (0);
+
+}
