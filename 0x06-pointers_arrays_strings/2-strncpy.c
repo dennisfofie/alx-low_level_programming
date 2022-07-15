0c33c330f882daaed1b204a2a74038e65664cea7
dennisfofie <dennisbeckham000@gmail.com>
2022-07-14 18:10:21 -0700

    hot fix betty coding style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strncpy - copy two strings
+ *
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: strings character
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
