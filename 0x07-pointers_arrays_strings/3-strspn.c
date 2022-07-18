e12fa8247679bbaca61a07afd539abd0a580e135
dennisfofie <dennisbeckham000@gmail.com>
2022-07-18 03:56:09 -0700

    finding the lenght of string in the other

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - calculate the initial lenght of a from another string
+ *
+ * @s: string to calculate from other
+ * @accept: string to index
+ *
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = strspn(s, accept);
+	return (len);
+}
