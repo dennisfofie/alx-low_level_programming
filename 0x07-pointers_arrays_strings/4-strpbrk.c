927a3a5e8398fdb3e269ba20a09315557ea5353e
dennisfofie <dennisbeckham000@gmail.com>
2022-07-18 04:06:11 -0700

    using strpbrk to find the first occurence of a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - find the first occurence of any character in string
+ *
+ * @s: first argument
+ * @accept: second argument
+ *
+ * Return: a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	len = strpbrk(s, accept);
+	return (len);
+}
