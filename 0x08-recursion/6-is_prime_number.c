048a2d9eb0ec2191355677a170e14d849fb8f9c2
dennisfofie <dennisbeckham000@gmail.com>
2022-07-20 15:56:04 -0700

    finding whether a number is a prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,15 @@
  */
 int is_prime_number(int n)
 {
-	if (n / 1 == n && n / n == 1)
+	if (n > 0)
 	{
-		return (1);
-	}
-	else
-	{
-		return (0);
+		if (n / 1 == n && n / n == 1)
+		{
+			return (1);
+		}	
+		else
+		{
+			return (0);
+		}
 	}
 }
