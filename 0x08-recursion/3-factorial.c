d75c679fd5f961afeea730ddc5270bb132d1e752
dennisfofie <dennisbeckham000@gmail.com>
2022-07-20 14:55:55 -0700

    finding the factorial of integer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - finds nth factorial
+ *
+ * @n: integer param
+ * Return: a factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
