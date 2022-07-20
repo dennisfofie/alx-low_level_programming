842256e02911da2a813f513cd1e8c5f526fb1572
dennisfofie <dennisbeckham000@gmail.com>
2022-07-20 15:16:57 -0700

    using recursion to solve power of integer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - cal the power of int
+ *
+ * @x:base
+ * @y:exponent
+ *
+ * Return: integer power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x));
+	}
+}
