f3dcf4e79d701c5d2050276d571db93ba651fb17
dennisfofie <dennisbeckham000@gmail.com>
2022-07-20 15:50:07 -0700

    finding whether a number is a prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_prime_number - print a prinme number
+ *
+ * @n: the number to checked
+ *
+ * Return: prime number
+ */
+int is _prime_number(int n)
+{
+	if (n / 1 == n && n / n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
