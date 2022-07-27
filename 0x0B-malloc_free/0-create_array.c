0f1c8147027ec5eb49d2656f1d7cca07ff769e43
dennisfofie <dennisbeckham000@gmail.com>
2022-07-27 01:24:37 -0700

    creating array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - the function to create the array
+ *
+ * @size: The size of the array
+ * @c: The character to be init
+ * Return: character strings
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	s = (char*)(size * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
