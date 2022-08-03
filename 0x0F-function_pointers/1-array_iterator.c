2727270685625257bc7984587c83e80eb8cc8e37
dennisfofie <dennisbeckham000@gmail.com>
2022-08-03 16:16:14 -0700

    print array with function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 		{
 			int i;
 
-			if (action)
+			if (action && size && arary)
 			{
 				for (i = 0; i < (int)size; i++)
 				{
