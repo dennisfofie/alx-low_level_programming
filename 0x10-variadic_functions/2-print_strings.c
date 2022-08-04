84eede6bc7deaaed15e094b001dfc178ccc0a2c8
dennisfofie <dennisbeckham000@gmail.com>
2022-08-04 16:23:51 -0700

    using variadic function to create string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,8 +18,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		p = va_arg(list, char*);
-		
-		if ( p == NULL)
+
+		if (p == NULL)
 		{
 			printf("%s", p);
 		}
@@ -31,5 +31,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	printf("\n");
 	va_end(list);
-	
 }
