468b6796046c90b18fc1d233c56b8b96f78aced3
dennisfofie <dennisbeckham000@gmail.com>
2022-08-16 14:24:10 +0000

    printing elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* print_listint - print the element of a list
+* @h: head of the list
+* Return: the number of element
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint *ptr;
+
+	ptr = h;
+	
+	while (ptr)
+	{
+		printf("%d\n", ptr->n);
+		count += 1;
+		ptr = ptr->next;
+	}
+	return (count);
+	
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+#singly linked list project
+This project is mainly about how to apply singly linked list data structures in c programming
+It includes adding , deleting, transversing a link and reversing a link
