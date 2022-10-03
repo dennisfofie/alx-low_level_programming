bfbb31af728a89d1b16d2cac93524c5b5f13167c
dennisfofie <dennisbeckham000@gmail.com>
2022-10-03 21:46:17 +0000

    done with the advanced

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - sum of two int
+ * @a: first num
+ * @b: second num
+ * Return - on sucess return a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction of two nums
+ * @a: first num
+ * @b: second num
+ * Return - on sucess return a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication of a and b
+ * @a: first num
+ * @b: second num
+ * Return - return multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division of a /b
+ * @a: first num
+ * @b: second num
+ * Return - return a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod of a and b
+ * @a: first num
+ * @b: second num
+ * Return - return mod of a % b
+ */
+int mod(int a,  int b)
+{
+	return (a % b);
+}
+int main(void)
+{
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/maths.h
@@ -0,0 +1,9 @@
+#ifndef _MATHS_H
+#define _MATHS_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mod(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+#endif
